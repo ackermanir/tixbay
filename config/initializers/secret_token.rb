af2a08b2085a39b8d503e2e57492d6ad1195c84a
Ian Ackerman <ackermanir@gmail.com>
2012-10-11 18:09:53 -0700

    Added all rails generated files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tixbay::Application.config.secret_token = '07698dc722a1047de02ca9856c49ebaa219481174449afe72398dbcddff10dd8a9ca7b1015a12e1e46b29f4fe4e60d1d7dea587e24a8ec059099cea3a7405136'
