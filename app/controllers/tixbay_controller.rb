03c7148a10cfd2d4039a439d0621f3720db3b8ca
Terence <terencetam@berkeley.edu>
2012-10-20 01:39:23 -0700

    DRY out view framework with navbar and body partials

--- app/controllers/tixbay_controller.rb
+++ app/controllers/tixbay_controller.rb
@@ -1,8 +1,21 @@
 class TixbayController < ApplicationController
   def index
+	redirect_to "theater"
+  end
+  def theater 
+	@title = "Theater"
+	render :body
   end
   def music
+	@title = "Music"
+	render :body
   end
   def film
+	@title = "Films"
+	render :body 
+  end
+  def all_culture
+	@title = "All Cultures"
+	render :body
   end
 end
