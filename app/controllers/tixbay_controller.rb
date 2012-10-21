dd96a35179bf0da774560b6680c9b0c3617c38ee
Brittany Cheng <bcheng42@gmail.com>
2012-10-21 13:26:43 -0700

    fix some alignment issues

--- app/controllers/tixbay_controller.rb
+++ app/controllers/tixbay_controller.rb
@@ -3,22 +3,22 @@ class TixbayController < ApplicationController
 	redirect_to :action => :theater
   end
   def theater 
-	@title = "Theater"
+	@title = "theater"
 	@shows = Category.find_by_name("Theater").shows
 	render :body
   end
   def music
-	@title = "Music"
+	@title = "music"
 	@shows = Category.find_by_name("Jazz").shows
 	render :body
   end
   def film
-	@title = "Films"
+	@title = "film"
 	@shows = Category.find_by_name("Film").shows
 	render :body 
   end
   def all_culture
-	@title = "All Cultures"
+	@title = "all culture"
 	render :body
   end
 end
