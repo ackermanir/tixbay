eaa6fc03417750ed5236bade35aa379c30876250
Ian Ackerman <ackermanir@gmail.com>
2012-10-23 21:27:28 -0700

    Added full information to listings

--- app/models/show.rb
+++ app/models/show.rb
@@ -16,5 +16,51 @@ class Show < ActiveRecord::Base
       ev.process_event
     end
   end
-  
+
+  #returns formated string of prices specified by whose
+  def price_format(whose)
+    #Helper method
+    def cent_string(cents)
+      if cents == 0
+        return "Free"
+      elsif cents == -1
+        return "Sold Out"
+      end
+      output = (cents % 100).to_s
+      if output.length != 2
+        output += '0'
+      end
+      output = "$" + (cents / 100).to_s + "." + output
+      return output
+    end
+
+    low = cent_string(our_price_range_low)
+    high = cent_string(our_price_range_high)
+    if whose == 'full'
+      low = cent_string(full_price_range_low)
+      high = cent_string(full_price_range_high)
+    end
+    output = low
+    if high != low
+      output = "#{output} - #{high}"
+    end
+    return output
+  end
+
+  #returns a string from the first to last date for this show
+  def date_string
+    times = []
+    showtimes.all.each do |time|
+      times << time.date_time
+    end
+    times = times.sort
+    first_date = times.first.to_date.strftime('%m/%d')
+    last_date = times.last.to_date.strftime('%m/%d')
+    output = first_date 
+    if last_date != first_date
+      output += " - " + last_date
+    end
+    return output
+  end
+
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,3 +1,11 @@
 class Venue < ActiveRecord::Base
   has_many :shows
+
+  #Gives one line address of venue
+  def full_address
+    output = street_address + ", " + locality + ", " +
+      region + " " + postal_code.to_s
+    return output
+  end
+
 end
