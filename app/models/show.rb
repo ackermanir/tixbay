4286255f4f76a42432cffda1273f2f74faa59f30
Terence <terencetam@berkeley.edu>
2012-10-23 21:52:29 -0700

    crude distance checks to get a list of shows implemented

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,6 +2,11 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'net/http'
+require 'mathn'
+
+def to_radians angle
+  angle/180 * Math::PI
+end
 
 class Show < ActiveRecord::Base
   has_and_belongs_to_many :categories
@@ -9,27 +14,33 @@ class Show < ActiveRecord::Base
   has_many :showtimes, :dependent => :destroy
 
   #Method to call to parse all xml listings and add to database
-  def self.fill_from_web
-    raw = File.open(File.join(Rails.root, "app", "data", "listings.xml"))
-    xml_doc = Nokogiri::XML(raw)
-    xml_doc.xpath("//event").each do |event|
-      ev = Event.new(event)
-      ev.process_event
-    end
-  end
-
-  def self.get_closest_show
-  	url = "http://maps.googleapis.com/maps/api/geocode/xml?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&sensor=true"
+  def self.get_closest_shows(street_address, locality, region, postal_code)
+  	url = "http://maps.googleapis.com/maps/api/geocode/xml?address="
+    url += (street_address.gsub /\s+/, '+')  + ","
+	url += (locality.gsub /\s+/, '+')  + ","
+    url += region + "+" + postal_code.to_s
+    url +=  "&sensor=true"
     resp = Net::HTTP.get_response(URI.parse(url))
     data = resp.body
-	xml_doc = Hpricot::XML(data)
-	xml_doc.search(:result).each do |event|
-      puts event
-	end  
+	xml_doc = Nokogiri::XML(data)
+	location = xml_doc.xpath("//location")
+	myLat = location.xpath("lat").inner_html.to_f
+    myLong = location.xpath("lng").inner_html.to_f
+
+	#failed attempt to calculate the distance inside database 
+    #shows = Venue.find_all_by_sql("SELECT id, locality, ( 3959 * acos( cos( radians(37) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(-122) ) + sin( radians(37) ) * sin( radians( lat ) ) ) ) AS distance FROM shows HAVING distance < 25 ORDER BY distance LIMIT 0 , 20;") 
+	
+	result = []
+	Show.find(:all).each do |s|
+		v = Venue.find(s.venue_id)
+		if Math.sqrt((69*(myLat - v.geocode_latitude))**2 + (69*(myLong - v.geocode_longitude))**2) < 25 
+			result << s
+		end
+	end	
+	result 
   end
 
   #might move this in venue, so we can cache the geocoding without using up the quota
-  #even though json might be better, we had been using xml already
   def get_geocoding
 	venue = Venue.find(self.venue_id)
   	url = "http://maps.googleapis.com/maps/api/geocode/xml?address="
@@ -39,9 +50,9 @@ class Show < ActiveRecord::Base
     url +=  "&sensor=true"
     resp = Net::HTTP.get_response(URI.parse(url))
     data = resp.body
-	xml_doc = Hpricot::XML(data)
-	location = xml_doc.search(:location)
-	location.search(:lat).inner_html + "," + location.search(:lng).inner_html
+	xml_doc = Nokogiri::XML(data)
+	location = xml_doc.xpath("//location")
+	location.xpath("lat").inner_html + "," + location.xpath("lng").inner_html
   end
   
 end
