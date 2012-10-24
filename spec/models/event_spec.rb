0906bffa8a6487bd207a486ea218dc8dbf0281c9
Ian Ackerman <ackermanir@gmail.com>
2012-10-23 19:10:47 -0700

    Whitelist category filter. TBA dates time_note ignored

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -11,7 +11,7 @@ describe Event do
   end
   describe "process_event" do
     it "works" do
-      input = %Q{<listings><event id="46404"><deal_of_the_day>today_only</deal_of_the_day><full_price_range>$17.00</full_price_range><summary_as_text>Show twice.</summary_as_text><image>improv.jpg</image><venue><capacity>194</capacity><geocode_longitude>-122.433704</geocode_longitude><image>front.jpg</image><geocode_latitude>37.805033</geocode_latitude><link>theater</link><address><locality>San Francisco</locality><country_name>United States</country_name><postal_code>94123</postal_code><street_address>Marina Blvd At Buchanan St</street_address><region>CA</region></address><name>Bayfront Theater</name></venue><headline_as_text>BATS Improv</headline_as_text><link>sub=46404</link><upcoming_dates><event_date id="619189"><time_note>8:00pm (Improvised Horror Musical)</time_note><date>2012-10-20</date></event_date></upcoming_dates><title_as_text>BATS Improv Comedy</title_as_text><sold_out>false</sold_out><category_list><category id="1"><name>Comedy</name></category></category_list><our_price_range>FREE - $8.50</our_price_range></event></listings>}
+      input = %Q{<listings><event id="46404"><deal_of_the_day>today_only</deal_of_the_day><full_price_range>$17.00</full_price_range><summary_as_text>Show twice.</summary_as_text><image>improv.jpg</image><venue><capacity>194</capacity><geocode_longitude>-122.433704</geocode_longitude><image>front.jpg</image><geocode_latitude>37.805033</geocode_latitude><link>theater</link><address><locality>San Francisco</locality><country_name>United States</country_name><postal_code>94123</postal_code><street_address>Marina Blvd At Buchanan St</street_address><region>CA</region></address><name>Bayfront Theater</name></venue><headline_as_text>BATS Improv</headline_as_text><link>sub=46404</link><upcoming_dates><event_date id="619189"><time_note>8:00pm (Improvised Horror Musical)</time_note><date>2012-10-20</date></event_date></upcoming_dates><title_as_text>BATS Improv Comedy</title_as_text><sold_out>false</sold_out><category_list><category id="1"><name>Film</name></category></category_list><our_price_range>FREE - $8.50</our_price_range></event></listings>}
       File.stub(:open).and_return(input)
       Show.fill_from_web
       Show.fill_from_web
@@ -30,6 +30,7 @@ describe Event do
       ev = Event.new("help")
       ev.stub(:previous_record_show)
       ev.stub(:save_show)
+      ev.stub(:category_whitelist).and_return(true)
       ev.should_receive(:create_venue_record)
       ev.should_receive(:create_showtime_records)
       ev.should_receive(:create_show_record)
@@ -55,6 +56,14 @@ describe Event do
       hash[:cat].should == "hl"
     end
   end
+  describe "category_whitelist" do
+    it "return false if category not in list" do
+      input = %Q{<event id="46404"><category_list><category id="1"><name>Comedy</name></category></category_list></event>}
+      xml = Nokogiri::XML(input)
+      ev = Event.new(xml.xpath('event').first)
+      ev.category_whitelist.should == false
+    end
+  end
   describe "event_dates" do
     it "should return info in upcoming dates xml into array" do
       xml = Nokogiri::XML('<upcoming_dates><event_date id="619189">' +
@@ -63,6 +72,14 @@ describe Event do
       ev = Event.new(xml)
       ar = ev.event_dates
       ar[0].should == [619189, "2012-10-20 8:00pm (Improvised Horror Musical)"]
+    end
+    it "should handle TBA dates by getting rid of time" do
+      xml = Nokogiri::XML('<upcoming_dates><event_date id="619189">' +
+                         '<time_note>Date TBA: Two-Day Pass' +
+                         '</time_note><date>2012-10-20</date></upcoming_dates>')
+      ev = Event.new(xml)
+      ar = ev.event_dates
+      ar[0].should == [619189, "2012-10-20 "]
     end
   end
   describe "match_prices" do
