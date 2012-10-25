c556d994349b621fe14a951f05a28491a2d50ccd
Christina Guo <christinaguo@berkeley.edu>
2012-10-25 01:44:02 -0700

    recommendations controller

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,13 +1,30 @@
 class RecommendationsController < ApplicationController
+
+  # POST /recommendations
+  # when submit form
   def index
+    # @listings = processed form
+    # if logged in, save answers
+    # render :recommendations
   end
+
+  # GET /recommendations
+  # when click on 'custom'
+  def custom
+    # if not logged in or has no saved answers
+    redirect_to :actions=>"login"
+    # @listings = based on user
+    # render :recommendations
+  end
+
+  #should POST to index
   def form
     @title = "Recommendation Form"
     # @user =
   end
-  def signup
+
+  def login
     #if logged in:
     redirect_to :action=>"form"
-    #else
   end
 end
