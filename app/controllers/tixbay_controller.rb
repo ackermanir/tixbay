ac0df07d8a8b261bcf2932e49b622b0a97298f51
Terence <terencetam@berkeley.edu>
2012-10-27 10:18:32 -0700

    fixed location filtering method to get a list of shows as input

--- app/controllers/tixbay_controller.rb
+++ app/controllers/tixbay_controller.rb
@@ -39,17 +39,19 @@ class TixbayController < ApplicationController
   def preference
 	@title = "nearby shows"
 	#default value
-	@street_address = "2111 Bancroft Way"
-	@city = "Berkeley"
-	@region = "CA"
-	@zip_code = 94704
-	if request.post?
-		@street_address = params[:street_address]
-		@city = params[:city]
-		@region = params[:region]
-		@zip_code = params[:zip_code]
+    @location = {}
+	@location["street_address"] = "2111 Bancroft Way"
+	@location["city"] = "Berkeley"
+	@location["region"] = "CA"
+	@location["zip_code"] = 94704
+	if params.has_key?("street_address") 
+	    @location["street_address"] = params[:street_address] 
+    	@location["city"] = params[:city]
+	    @location["region"] = params[:region] 
+	    @location["zip_code"] = params[:zip_code]
 	end
-	@shows,@distances = Show.get_closest_shows(@street_address, @city, @region, @zip_code)
+    shows = Show.all
+	@shows = Show.get_closest_shows(shows, @location)
 	render :body
   end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -20,50 +20,34 @@ class Show < ActiveRecord::Base
     end
   end
 
-  def self.get_closest_shows(street_address, locality, region, postal_code)
-  	url = "http://maps.googleapis.com/maps/api/geocode/xml?address="
-    url += (street_address.gsub /\s+/, '+')  + ","
-	url += (locality.gsub /\s+/, '+')  + ","
-    url += region + "+" + postal_code.to_s
+  def self.get_closest_shows(shows, location)
+    url = "http://maps.googleapis.com/maps/api/geocode/xml?address="
+    url += (location["street_address"].gsub /\s+/, '+')  + ","
+    url += (location["city"].gsub /\s+/, '+')  + ","
+    url += location["region"] + "+" + location["zip_code"].to_s
     url +=  "&sensor=true"
     resp = Net::HTTP.get_response(URI.parse(url))
     data = resp.body
-	xml_doc = Nokogiri::XML(data)
-	location = xml_doc.xpath("//location")
-	myLat = location.xpath("lat").inner_html.to_f
+    xml_doc = Nokogiri::XML(data)
+    location = xml_doc.xpath("//location")
+    myLat = location.xpath("lat").inner_html.to_f
     myLong = location.xpath("lng").inner_html.to_f
 
-	#failed attempt to calculate the distance inside database 
-    #shows = Venue.find_all_by_sql("SELECT id, locality, ( 3959 * acos( cos( radians(37) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(-122) ) + sin( radians(37) ) * sin( radians( lat ) ) ) ) AS distance FROM shows HAVING distance < 25 ORDER BY distance LIMIT 0 , 20;") 
-	
-	result = []
-	distances = {}
-	Show.find(:all).each do |s|
-		v = Venue.find(s.venue_id)
-		distance = Math.sqrt((69*(myLat - v.geocode_latitude))**2 + (69*(myLong - v.geocode_longitude))**2)
-		if distance < 25 
-			distances[s.id] = distance
-			result << s
-		end
-	end	
-	[result, distances]
+    result = []
+    shows.each do |s|
+        distance = s.get_distance(myLat, myLong)
+        if distance < 25 
+            result << s
+        end
+    end 
+    result
   end
 
-  #might move this in venue, so we can cache the geocoding without using up the quota
-  def get_geocoding
-	venue = Venue.find(self.venue_id)
-  	url = "http://maps.googleapis.com/maps/api/geocode/xml?address="
-    url += (venue.street_address.gsub /\s+/, '+')  + ","
-	url += (venue.locality.gsub /\s+/, '+')  + ","
-    url += venue.region + "+" + venue.postal_code.to_s
-    url +=  "&sensor=true"
-    resp = Net::HTTP.get_response(URI.parse(url))
-    data = resp.body
-	xml_doc = Nokogiri::XML(data)
-	location = xml_doc.xpath("//location")
-	location.xpath("lat").inner_html + "," + location.xpath("lng").inner_html
+  def get_distance(myLat,myLong) 
+     v = Venue.find(self.venue_id)
+     #approximate distance in miles by latitude/longitude degrees
+     Math.sqrt((69*(myLat - v.geocode_latitude))**2 + (69*(myLong - v.geocode_longitude))**2)
   end
-
 
   #returns formated string of prices specified by whose
   def price_format(whose)
