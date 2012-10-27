fc555f0faaf699b0ddbee31daab7a43448750d5c
Ian Ackerman <ackermanir@gmail.com>
2012-10-27 16:28:59 -0700

    DRYed out the controller by moving method to category.

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,15 @@
+# spec/models/show_spec.rb
+require 'spec_helper'
+require 'rspec-rails'
+
+describe Category do
+  describe "self.shows_from_category" do
+    it "calls from all files specified by type" do
+      sh = mock('showtimes')
+      sh.stub(:shows).and_return(["l"])
+      Category.should_receive(:where).
+        with(:name => ['Theater', 'Performing Arts']).and_return([sh])
+      Category.shows_from_category('theater')
+    end
+  end
+end
