07bcfa29257e7668b856a5e9f5974104f7f9f0c6
Terence <terencetam@berkeley.edu>
2012-10-28 14:50:03 -0700

    added geolocations spec tests

--- spec/models/show_spec.rb
+++ spec/models/show_spec.rb
@@ -19,7 +19,32 @@ describe Show do
       Show.fill_from_xml
     end
   end
+  describe "get_closest_shows" do
+    it "should go through a list of shows and call get_distances" do
+      fake_show = mock('show')
+      fake_show.should_receive(:get_distance).exactly(2).times.and_return(23.0)
+      fake_shows = [fake_show, fake_show]
 
+      location = {}
+      location['street_address'] = "2111 Bancroft Way"
+      location['city'] = "Berkeley"
+      location['region'] = "CA"
+      location['zip_code'] = 94709 
+      Show.get_closest_shows(fake_shows,location) 
+    end
+  end
+  describe 'get_distance' do
+    it 'should return a distance' do
+      fake_venue = mock('venue')
+      fake_venue.stub(:geocode_latitude).and_return(19)
+      fake_venue.stub(:geocode_longitude).and_return(20)
+      Venue.stub(:find).and_return(fake_venue)
+      fake_show = Show.new() 
+      fake_show.stub(:venue_id).and_return(1)
+      #fake_show.should_receive(:get_distance).and_return(0)
+      fake_show.get_distance(20,20).should > 0
+    end
+  end
   describe "price_format" do
     it "Handles sold out shows" do
       s = Show.new
@@ -40,7 +65,6 @@ describe Show do
       s.price_format("full").should == "$33.33 - $65.23"
     end
   end
-
   describe "date_string" do
     it "Handles one date" do
       s = Show.new
