5d1958f77b64e7933c19bb85d6b20ebb7c5c7bdf
Ian Ackerman <ackermanir@gmail.com>
2012-11-03 20:00:58 -0700

    Added general filtering from form

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,14 +1,21 @@
 class Category < ActiveRecord::Base
   has_and_belongs_to_many :shows
+
+  @@category_types = {
+    "theater" => ['Theater', 'Performing Arts'],
+    "music" => ['Popular Music', 'Jazz', 'Classical', 'Classic Rock'],
+    "film" => ['Film'],
+    "all culture" => ['Theater', 'Performing Arts', 'Popular Music', 'Jazz',
+                      'Classical', 'Classic Rock', 'Film']
+  }
+
+  def self.all_categories
+    return @@category_types["all culture"]
+  end
   
   def self.shows_from_category(page)
-    category_types = {"theater" => ['Theater', 'Performing Arts'],
-      "music" => ['Popular Music', 'Jazz', 'Classical', 'Classic Rock'],
-      "film" => ['Film'],
-      "all culture" => ['Theater', 'Performing Arts', 'Popular Music', 'Jazz',
-                        'Classical', 'Classic Rock', 'Film'] }
     shows = []
-    Category.where(:name => category_types[page]).each do |c|
+    Category.where(:name => @@category_types[page]).each do |c|
       shows += c.shows.to_a
     end
     return shows
--- app/models/show.rb
+++ app/models/show.rb
@@ -9,6 +9,14 @@ class Show < ActiveRecord::Base
   belongs_to :venue  
   has_many :showtimes, :dependent => :destroy
 
+  #Scopes used for recommendation filtering
+  scope :price_greater, lambda { |price|
+    {:conditions => ["our_price_range_high >= ?", price]} }
+  scope :price_lower, lambda { |price|
+    {:conditions => ["our_price_range_low <= ?", price]} }
+  scope :in_categories, lambda { |categories|
+    {:conditions => {'categories.name' => categories}} }
+
   #Method to call to parse all xml listings and add to database
   def self.fill_from_xml(location = File.join(Rails.root, "app", 
                                               "data", "listings.xml"))
@@ -47,6 +55,43 @@ class Show < ActiveRecord::Base
      v = Venue.find(self.venue_id)
      #approximate distance in miles by latitude/longitude degrees
      Math.sqrt((69*(myLat - v.geocode_latitude))**2 + (69*(myLong - v.geocode_longitude))**2)
+  end
+
+"""
+Filters and order of application:
+
+Query search:
+Price Range - Default to free through infinite or [0, -1]
+Category ID - Hard filtering, all optional, default to all categories
+
+Done on a show array:
+Address - hash with 'street_address', 'city', 'region', and 'zip_code',
+          zip code the only thing necessary
+Distance - miles, default to 10
+
+TODO: Keyword which only makes preferential searches
+
+"""
+  #Defaults to recommending all shows
+  def self.recommendShows(priceRange = [0, -1], 
+                          categories = Category.all_categories,
+                          location = nil, 
+                          distance = 10,
+                          keywords = [])
+    #Filter based on price and categories
+    shows = Show.price_greater(priceRange[0])
+    shows = shows.price_lower(priceRange[1]) unless priceRange[1] == -1
+    shows = shows.joins(:categories).in_categories(categories)
+    shows = shows.all
+
+    #TODO Pass in distance as well
+    shows = Show.get_closest_shows(shows, location) unless not location
+    return shows.first(1)
+  end
+
+  #returns the shows similar to the current show
+  def similar_shows
+    return nil
   end
 
   #returns formated string of prices specified by whose
