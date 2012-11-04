0348d95c4aeedbfd0f18903d3a1bc9e7632a3466
Ian Ackerman <ackermanir@gmail.com>
2012-11-04 14:34:18 -0800

    Created method to fix time for cucumbers

--- features/step_definitions/show_step.rb
+++ features/step_definitions/show_step.rb
@@ -1,3 +1,4 @@
+require 'timecop'
 
 Given /^the database has been setup with similarshows.xml$/ do
   Show.fill_from_xml(File.join(Rails.root, "app", "data", "similarshows.xml"))
@@ -5,4 +6,8 @@ end
 
 Given /^that (?:|I )am on the show page with id "(.+)"$/ do |id_val|
   visit path_to('the show page with id "' + id_val + '"')
+end
+
+Given /^the date is set to 2012$/ do
+  Timecop.travel(DateTime.parse("2012-10-01"))
 end
