bc9286d03494d7e1c045264bddffac5da2d37722
Cong Chen <congchen5@unknown7cd1c3f026e9.att.net>
2012-11-11 22:39:37 -0800

    commented out empty tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :shows
 
   validates :zip_code, :length => {:is => 5} #make sure the zip code given is 5 digits
+
+  def get_liked_shows
+    return self.shows
+  end
+
+  def get_preferred_categories
+    return self.categories
+  end
+
 end
