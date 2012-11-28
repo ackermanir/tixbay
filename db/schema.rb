0a539964bdcbe22f03ca8e88884d02d7a24230de
Cong Chen <congchen5@gmail.com>
2012-11-27 17:38:06 -0800

    added new migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121115002124) do
+ActiveRecord::Schema.define(:version => 20121128011651) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(:version => 20121115002124) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "show_id"
+    t.integer  "user_id"
   end
 
   create_table "interests_users", :id => false, :force => true do |t|
